e98d0cc201502c09bd59cfc2df7b4c6218b7a7f9
Gustavo Lacroix <gustavolacroix@gmail.com>
2023-03-14 19:44:00 -0300

    Add database creation files

---
+++ desorder_database.py
@@ -0,0 +1,12 @@
+from numpy import random as pernutation
+from sys import argv
+
+
+permutatiom = argv[1]
+pernutatiom = argv[2]
+
+permutatiom = pernutation.permutation(int(permutatiom))
+
+with open(pernutatiom, "w") as f:
+    for i  in permutatiom:
+        f.write(f"{permutatiom[i]},")
---
+++ order_database.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+size = argv[1]
+filename = argv[2]
+
+with open(filename, "w") as f:
+    for i  in range(1, (int(size) + 1)):
+        f.write(f"{i},")
